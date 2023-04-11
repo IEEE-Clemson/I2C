4d9a2dfbdc609922ea04480f74c323228177064b
Connor Belli <cbell@clemson.edu>
2023-04-11 18:42:01 -0400

    Added stepper state machine

--- slave/slave.ino
+++ slave/slave.ino
@@ -43,10 +43,37 @@ Servo servo2;
 Servo servo3;
 Servo servo4;
 
+// What state is the carriage in?
+// 0 -> idle
+// 1 -> Moving up
+// 2 -> Move down after hitting top
+// 3 -> Waiting for 4 seconds
+// 4 -> Moving down
+// 5 -> Move to position
+int carriageState = 0;
+int targetPosition = 0;
+// temp var for storing wait time
+long startTime = 0;
+
+void startLiftSequence() {
+  if(carriageState == 0) {
+    carriageState = 1;
+    stepper.moveTo(-40000);
+  }
+}
+
+void moveToPosition(int pos) {
+  if(carriageState == 0) {
+    targetPosition = pos;
+    stepper.moveTo(targetPosition);
+    carriageState = 5;
+  }
+}
+
 void setup(){
   // I2C setup
-  Wire.setSDA(0);
-  Wire.setSCL(1);
+  //Wire.setSDA(0); 
+  //Wire.setSCL(1);
   Wire.begin(0x08);
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
@@ -70,6 +97,7 @@ void setup(){
   // SETUP STEPPER;
   stepper.setMaxSpeed(1500);
   stepper.setAcceleration(2000);
+  startLiftSequence();
 }
 
 void receiveEvent(int x){
@@ -79,12 +107,16 @@ void receiveEvent(int x){
     red_light();
   }else if(msg==0x02){
     run_servo1();
+    Wire.write(0x04);
   }else if(msg==0x03){
     run_servo2();
+    Wire.write(0x04);
   }else if(msg==0x04){
     run_servo3();
+    Wire.write(0x04);
   }else if(msg==0x05){
     run_servo4();
+    Wire.write(0x04);
   }else if(msg == 0x06){
     move_carriage();
   }else if((msg & 0x0F) == 0x07){
@@ -95,7 +127,8 @@ void receiveEvent(int x){
     move_rollers(90);
   }else if(msg == 0x0A){
     stop_rollers();
-  }
+  } 
+
 }
 
 void red_light(){
@@ -172,66 +205,93 @@ void stop_rollers(){
   Wire.write(0x09);
 }
 
+
+
+void progressStepperState() {
+  switch(carriageState) {
+  case 0: // Idle, do nothing
+    break;
+  case 1: // We are doing the up motion, wait until we reach the top
+    if(stepper.currentPosition() < -6500/4) {
+      carriageState = 2;
+      stepper.setSpeed(0);
+      stepper.setCurrentPosition(-6450/4);
+      stepper.moveTo(-6300/4);
+    }
+    break;
+  case 2: // Moving down a little
+    
+    if(stepper.currentPosition() == -6300/4) {
+      carriageState = 3;
+      startTime = millis();
+    }
+    break;
+  case 3: // Wait 4 seconds
+    if(millis()-startTime > 4000) {
+      carriageState = 4;
+      stepper.moveTo(0);
+    }
+    break;
+  case 4: // Drive to bottom
+    if(stepper.currentPosition() == 0) {
+      carriageState = 0;
+    }
+    break;
+  case 5:
+    if(stepper.currentPosition() == targetPosition) {
+      carriageState = 0;
+    }
+  default: // Invalid state, reset to idle
+    carriageState = 0;
+  }
+  // Update the stepper
+  stepper.run();
+}
+
 void loop(){
   if(is_servo1){
     if(servo1_pos<360){
-      servo1.write((180*(servo1_pos>180))+((-1+2*(servo1_pos<=180))*(servo1_pos%180));     
+      //servo1.write((180*(servo1_pos>180))+((-1+2*(servo1_pos<=180))*(servo1_pos%180));     
       servo1_pos++;             
     }
     if(servo1_pos==360){
-      Wire.write(0x04);
+      
       servo1_pos = 0;
       is_servo1 = false;
     }
   }
   if(is_servo2){
     if(servo2_pos<360){
-      servo2.write((180*(servo2_pos>180))+((-1+2*(servo2_pos<=180))*(servo2_pos%180));     
+      //servo2.write((180*(servo2_pos>180))+((-1+2*(servo2_pos<=180))*(servo2_pos%180));     
       servo2_pos++;             
     }
     if(servo2_pos==360){
-      Wire.write(0x04);
+      //Wire.write(0x04);
       servo2_pos = 0;
       is_servo2 = false;
     }
   }
   if(is_servo3){
     if(servo3_pos<360){
-      servo3.write((180*(servo3_pos>180))+((-1+2*(servo3_pos<=180))*(servo3_pos%180));     
+      //servo3.write((180*(servo3_pos>180))+((-1+2*(servo3_pos<=180))*(servo3_pos%180));     
       servo3_pos++;             
     }
     if(servo3_pos==360){
-      Wire.write(0x04);
+      //Wire.write(0x04);
       servo3_pos = 0;
       is_servo3 = false;
     }
   }
   if(is_servo4){
     if(servo4_pos<360){
-      servo4.write((180*(servo4_pos>180))+((-1+2*(servo4_pos<=180))*(servo4_pos%180));     
+      //servo4.write((180*(servo4_pos>180))+((-1+2*(servo4_pos<=180))*(servo4_pos%180));     
       servo4_pos++;             
     }
     if(servo4_pos==360){
-      Wire.write(0x04);
+      //Wire.write(0x04);
       servo4_pos = 0;
       is_servo4 = false;
     }
   }
-  if(is_stepper){
-    stepper.moveTo(-40000);
-    while(stepper.currentPosition() > -6500/4) {
-      stepper.run();
-    }
-    stepper.setSpeed(0);
-    stepper.setCurrentPosition(-6450/4);
-    stepper.moveTo(-6450/4);
-    //stepper.runToPosition();
-    stepper.moveTo(-6300/4);
-    stepper.runToPosition();
-    delay(4000);
-    
-    stepper.moveTo(0);
-    stepper.runToPosition();
-    delay(10000);
-  }
+  progressStepperState();
 }
